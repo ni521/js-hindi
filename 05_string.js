04759bc45412fcb217a7390d6af04d8ccaec2ead
ni521 <119124479+ni521@users.noreply.github.com>
2024-04-30 11:36:10 +0530

    Add files via upload

---
+++ 05_string.js
@@ -0,0 +1,32 @@
+const name = "nitin"
+const repoCount = 50
+
+// console.log(name + repoCount + "value")
+
+console.log(`Hello my name is ${name} and myrepo countis ${repoCount}`);
+
+const gameName = new String('nitin')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf(`t`));
+
+const  newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+const newStringOne = "   nitin    "
+console.log(newStringOne);
+console.log(newStringOne.trim())
+
+const url = "https://nitin.com/nitin%20sharma"
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('nitin'))
